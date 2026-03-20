8b8eb84829165f9bf7891ac009bc444d2370446e
Crague Ilian <ilian.crague@etu.u-paris.com>
2026-03-20 22:18:11 +0100

    maps

--- src/main/java/fr/ensimag/classes/Map.java
+++ src/main/java/fr/ensimag/classes/Map.java
@@ -1,4 +1,32 @@
 package fr.ensimag.classes;
 
+import fr.ensimag.Displayable;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+
 public class Map {
+	private final ArrayList<Displayable> elements;
+
+	public Map() {
+		this.elements = new ArrayList<>();
+	}
+
+	public void addElement(Displayable element) {
+		elements.add(Objects.requireNonNull(element, "element cannot be null"));
+	}
+
+	public void removeElement(Displayable element) {
+		elements.remove(element);
+	}
+
+	public List<Displayable> getElements() {
+		return Collections.unmodifiableList(elements);
+	}
+
+	public int size() {
+		return elements.size();
+	}
 }
